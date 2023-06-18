7a574e12a1faa0f8d5cefe9800c462aaf5e60acf
diogocastros1 <diogoantonio@gec.inatel.br>
2023-06-18 10:53:59 -0300

    Atualização de estudante

--- inaSchool/student.py
+++ inaSchool/student.py
@@ -1,54 +1,12 @@
 class Student:
-    def __init__(self, name: str, curse: str):
+    def __init__(self, register_number: int, name: str, curse: str):
+        self.register_number = register_number
         self.name = name
         self.curse = curse
-        self.matters = {}
         
     def to_dict(self):
         return {
+            "register_number": self.register_number,
             "name": self.name,
             "students": self.curse,
-            "teachers": self.teachers,
         }
-        
-class StudentDAO:
-    def __init__(self,database):
-        self.db = database
-        
-    def create(self, matter):
-        try:
-            res = self.db.collection.insert_one(matter.__dict__)
-            print(f'Motorista adicionado com sucesso! {res.inserted_id}')
-            return res.inserted_id
-        except Exception as e:
-            print(f'Erro ao adicionar motorista: {e}')
-            return None
-        
-    def read(self, name: str):
-        try:
-            res = self.db.collection.find_one({'name': name})
-            print(f'Materia encontrada!')
-            return res
-        except Exception as e:
-            print(f'Erro na leitura da materia: {e}')
-            return None
-        
-    def update(self, old_name: str, new_name: str):
-        try:
-            res = self.db.collection.find_one({'name': old_name})
-            self.db.collection.update_one({'_id':res._id}, {'$set':{'name': new_name}})
-            print(f'Nome atualizado!')
-            return res
-        except Exception as e:
-            print(f'Erro ao atualizar: {e}')
-            return None
-    
-    def delete(self, name: str):
-        try:
-            res = self.db.collection.delete_one({'name': name})
-            print(
-                f'Materia deletada: {res.deleted_count} documento deletado')
-            return res.deleted_count
-        except Exception as e:
-            print(f'Erro ao deletar motorista: {e}')
-            return None
